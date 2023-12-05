95f628e9c440903cbdee193b0acd3c0970ee3637
Nicolas Vycas Nery <vycasnicolas@gmail.com>
2023-12-05 06:05:24 +0000

    Refactor Coordinator and Process classes

--- app/__main__.py
+++ app/__main__.py
@@ -2,7 +2,6 @@ import logging
 from rich.logging import RichHandler
 from coordinator import Coordinator
 from process import Process
-import socket
 
 format="%(asctime)s (%(threadName)-2s) %(message)s"
 logging.basicConfig(
@@ -12,17 +11,17 @@ logger = logging.getLogger(__name__)
 
 # Create a Coordinator object
 coordinator_host = ('localhost', 12345)
-coordinator = Coordinator(coordinator_host[0], coordinator_host[1])
+coordinator = Coordinator(coordinator_host)
 logger.info("Coordinator created")
 
 # Start the Coordinator server
-coordinator.start()
+coordinator.start_server()
 logger.info("Coordinator server started")
 
 # Create 5 Process clients and connect them to the Coordinator
 processes = []
 for i in range(5):
-    process = Process(i, coordinator_host)
+    process = Process(str(i), coordinator_host)
     processes.append(process)
     process.start()
     logger.info(f"Process {i} started")
