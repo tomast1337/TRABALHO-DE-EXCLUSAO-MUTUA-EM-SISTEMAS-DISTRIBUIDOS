bf66dc0dfc41fc1679d6b86b8a112709593c902c
Nicolas Vycas Nery <vycasnicolas@gmail.com>
2023-12-06 21:13:54 +0000

    Add multithreading support for coordinator

--- app/supertest.py
+++ app/supertest.py
@@ -1,7 +1,8 @@
 import logging
+from threading import Thread
+import time
 import Pyro4
 from rich.logging import RichHandler
-from coordinator import Coordinator
 from process import Process
 
 format="%(asctime)s (%(threadName)-2s) %(message)s"
@@ -19,13 +20,15 @@ if quantity > 9 or quantity < 0:
     logger.error("Invalid quantity. Quantity must be between 0 and 9")
     exit()
 
-def main():
-    daemon = Pyro4.Daemon()
-    logger.info("Coordinator starting...")
-    uri = daemon.register(Coordinator)
-    logger.info(f"Coordinator uri: {uri}")
-    daemon.requestLoop()
 
+uri = input("Enter the uri of the coordinator: ").strip()
+
+if uri == "":
+    logger.error("Invalid uri")
+    exit()
+
+
+def main_processs():
     # create a process for each process id
     processes = []
     for i in range(quantity):
@@ -42,6 +45,6 @@ def main():
         process.join()
 
     logger.info("Processes closed")
-
+    
 if __name__=="__main__":
-    main()
+    Thread(target=main_processs).start()
