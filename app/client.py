cd118b11a1f8c1b0d8779f248da8e74b352b59ce
Nicolas Vycas Nery <vycasnicolas@gmail.com>
2023-12-06 20:40:42 +0000

    Add Pyro4 import and update process initialization

--- app/client.py
+++ app/client.py
@@ -2,6 +2,7 @@ import logging
 from rich.logging import RichHandler
 from coordinator import Coordinator
 from process import Process
+import Pyro4
 
 format="%(asctime)s (%(threadName)-2s) %(message)s"
 logging.basicConfig(
@@ -9,13 +10,25 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-coordinator_host = ('localhost', 12345)
-# Create 5 Process clients and connect them to the Coordinator
+id = int(input("Enter process id: "))
 
-id = input("Enter process id: ")
+if id > 9 or id < 0:
+    logger.error("Invalid process id. Process id must be between 0 and 9")
+    exit()
 
-process = Process(str(id), coordinator_host)
+uri = input("Enter the uri of the coordinator: ").strip()
+
+if uri == "":
+    logger.error("Invalid uri")
+    exit()
+
+
+process = Process(
+        process_id = str(id),
+        coordinator = Pyro4.Proxy(uri),
+        filename="processes.log"
+)
+logger.info(f"Process {id} starting...")
 process.start()
-logger.info(f"Process {id} started")
 process.join()
 logger.info("Processes closed")
