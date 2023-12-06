7745683e1cf5ec64e96df3791cfdbc91f77fdc0e
Nicolas Vycas Nery <vycasnicolas@gmail.com>
2023-12-06 20:39:47 +0000

    Refactor process.py to use Pyro4 for communication

--- app/process.py
+++ app/process.py
@@ -2,56 +2,67 @@ import threading
 import time
 import logging
 from message import Message , MessageTypes
-import socket
 import time
 from datetime import datetime
+import Pyro4
+import random
 
 class Process(threading.Thread):
-    def __init__(self, process_id, coordinator_address, filename="processes.txt"):
+    def __init__(self, 
+                 process_id:str,
+                    coordinator:Pyro4.Proxy,
+                    filename:str
+                    ):
         threading.Thread.__init__(self)
         self.process_id = process_id
-        self.coordinator_address = coordinator_address
+        self.coordinator = coordinator
         self.filename = filename
         self._logger = logging.getLogger(__name__)
 
-    def work(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(self.coordinator_address)
-        self._logger.info(f"Process {self.process_id} connected to coordinator")
-        try:
-            while True:  # repeat indefinitely
-                request_msg = Message('1', self.process_id, MessageTypes.REQUEST).create_message()
-                s.sendall(request_msg.encode())
-                self._logger.info(f"Process {self.process_id} sent REQUEST message")
-                self.write_to_file("REQUEST")
-                while True:  # wait for GRANT message
-                    data = s.recv(1024)
-                    if not data:
-                        continue
-                    response_msg = Message.from_string(data.decode())
-                    if response_msg.messageType == MessageTypes.GRANT:
-                        self._logger.info(f"Process {self.process_id} received GRANT message")
-                        self.write_to_file("GRANT")
-                        time.sleep(5)  # wait for 2 seconds
-                        release_msg = Message('2', self.process_id, MessageTypes.RELEASE).create_message()
-                        self.write_to_file("RELEASE")
-                        s.sendall(release_msg.encode())
-                        self._logger.info(f"Process {self.process_id} sent RELEASE message")
-                        break  # break the loop after receiving GRANT message
-        except (BrokenPipeError, ConnectionResetError):
-            self._logger.error(f"Process {self.process_id} was disconnected from coordinator")
-        finally:
-            s.close()
-            
     def run(self):
-        try:
-            self.work()
-        except Exception as e:
-            self._logger.error(f"Process {self.process_id} error: {e}")
-        except KeyboardInterrupt:
-            self._logger.info(f"Process {self.process_id} interrupted")
-        else:
-            self._logger.info(f"Process {self.process_id} finished")
+        while True:
+            try:
+                self.work()
+                time.sleep(1)
+            except KeyboardInterrupt:
+                self._logger.info(f"Process {self.process_id} interrupted")
+                break
+
+        
+    def work(self):
+        # Request access message
+        self.write_to_file(MessageTypes.REQUEST)
+        message = Message(
+                messageID = "1",
+                processID = self.process_id,
+                messageType = MessageTypes.REQUEST
+        ).create_message()
+
+        # wait for GRANT message
+        response = None
+        while True:
+            response = self.coordinator.handle_message(message)
+            if response is None:
+                self._logger.info(f"Process {self.process_id} waiting for access")
+                time.sleep(1)
+            else:
+                break
+
+        # write to file when access is granted
+        self.write_to_file(MessageTypes.GRANT)
+        
+        time.sleep(random.randint(3,5)) # simulate work in the critical section
+            
+        # Release access message
+        self.write_to_file(MessageTypes.RELEASE)
+        message = Message(
+                messageID = "1",
+                processID = self.process_id,
+                messageType = MessageTypes.RELEASE
+        ).create_message()
+        self.coordinator.handle_message(message)
+
+        time.sleep(random.randint(3,5)) # simulate work outside the critical section
 
     def write_to_file(self, message=""):
         with open(self.filename, 'a') as f:
